b99e1e587edb4bd0575e79af6e30d0e796d60c94
ziyodullodev <ziyodullodev@gmail.com>
2025-10-20 16:55:40 +0500

    barini qoshdim

--- main.py
+++ main.py
@@ -7,6 +7,23 @@ app = Flask("__name__")
 def home():
     return render_template("index.html")
 
+@app.route("/iskandar")
+def iskandar():
+    return render_template("iskandar.html")
+
+
+@app.route("/abdurahmon")
+def abdurahmon():
+    return render_template("abdurahmon.html")
+
+
+@app.route("/diyorbek")
+def diyorbek():
+    return render_template("tigr.html")
+
+@app.route("/iqbola")
+def iqbola():
+    return render_template("iqbola.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
